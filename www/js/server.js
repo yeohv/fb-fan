93332161fb64dc58b0d0c34bfe804707e2e51563
Vanessa <vanessa@meqo.com>
2015-10-19 18:08:26 +0800

    updated web

--- www/js/server.js
+++ www/js/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
-
-app.use(express.static(__dirname + "/www"));
-app.listen(process.env.PORT || 5000);
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
