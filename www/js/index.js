31a87012d7e038c1d24863adf37ad5d0391fa489
Vanessa <vanessa@meqo.com>
2015-10-23 18:24:36 +0800

    bugs

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,7 @@ app.factory('pullFb', function($http,$rootScope,$stateParams,$ionicLoading,$sce)
     analytics:function(e){
         ga('send','event','dev',e,$rootScope.brand);
         console.log("ga:"+e", "+$rootScope.brand);
-    },
+    }),
 		getPosts: function(){
       //console.log("getPosts:"+$rootScope.brand);
       query="/?fields=posts.fields(name,link,source,description,place,created_time,message,likes.limit(1).summary(true),comments.limit(1).summary(true),shares,picture)&";
