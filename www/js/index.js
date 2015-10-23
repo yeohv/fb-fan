786eb5fe0d082348f7c21e167b2aa837235dc85b
Vanessa <vanessa@meqo.com>
2015-10-23 14:34:51 +0800

    added email

--- www/js/index.js
+++ www/js/index.js
@@ -796,46 +796,6 @@ app.controller("brands",function($scope,$rootScope,$http,pullFb){
       });
       console.log($rootScope.brands);
     }
-
-    $scope.send=function(id,name,email){
-      var api_key = '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';
-    // using SendGrid's Node.js Library - https://github.com/sendgrid/sendgrid-nodejs
-    var sendgrid = require("sendgrid")(api_key);
-
-    var text= "Dear " + name + ",\n"
-    + "I am Vanessa from StartYourApp, an organization that helps businesses communicate better with their customers.We think we can help you engage your customers much better than Facebook pages and newsletters.\n\n"
-    +"What we are proposing is to help you create your own mobile app that helps your business :\n"
-    +"- Engage customers at least 3-4 times more than Facebook pages and newsletters\n"
-    +"- Gain more insights about the behaviour of your customers \n"
-    +"- Update users directly without other noise (e.g posts not related to your brands), anytime of the day\n"
-    +"- Engage without extra effort. We integrates the app with your current methods of customer communications \n\n"
-    +"Do take a look at the sample prototype of your app that we have created for you in this link"
-    +"http://fb-fan.herokuapp.com/#/demo/"+id+"\n"
-    +"Please feel free to play around with it.\n\n"
-    +"If you'd like to learn more about this prototype or you app, don't hesitate to ask me. You can also visit our website: the link is in my signature.\n"
-    +"What do you think? Do you want to give it a try?\n\n"
-    +"Kindest regards,\n"
-    +"Vanessa Yeoh\n"
-    +"Account manager\n"
-    +"www.startyourapp.org\n";
-
-
-    var email     = new sendgrid.Email({
-      to:       email,
-      from:     'vanessa@meqo.com',
-      fromname: 'StartyourApp',
-      subject:  'Start your own app'+name,
-      text:     text,
-      date:     new Date();
-    });
-    console.log("testgrid");
-    sendgrid.send(email, function(err, json) {
-      if (err) { return console.error(err); }
-      console.log(json);
-    });
-
-    }
-
 });
 
 app.controller("help",function($scope,$rootScope,$ionicLoading,pullFb, ModalService){
